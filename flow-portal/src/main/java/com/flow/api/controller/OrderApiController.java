c9d8c3d63f3ef98ff5568fcb22b36edab4b4bece
yinchuanhui <422099468@qq.com>
2017-08-16 20:29:49 +0800

    wq

--- flow-portal/src/main/java/com/flow/api/controller/OrderApiController.java
+++ flow-portal/src/main/java/com/flow/api/controller/OrderApiController.java
@@ -12,7 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseBody;  
 import org.springframework.web.bind.annotation.RestController;
 
 import com.flow.api.model.OrderRequest;
